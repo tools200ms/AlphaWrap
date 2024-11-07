5c21f4a9b05a16c9b7579fdff2e2d288c7dbd255
Mateusz Piwek <barnaba@200ms.net>
2024-11-07 14:09:24 +0100

    Code refactored by ChatGPT

--- alpha-wrap/composer.sh
+++ alpha-wrap/composer.sh
@@ -1,100 +1,77 @@
 #!/bin/sh
 
-[ -n "$PRETEND" ] && [[ $(echo "$PRETEND" | tr '[:upper:]' '[:lower:]') =~ ^y|yes|1|on$ ]] && \
-        RUN="echo" || RUN=
+# Function to check flags for pretend or debug mode
+check_flag() {
+    [ -n "$1" ] && echo "$1" | tr '[:upper:]' '[:lower:]' | grep -Eq '^y|yes|1|on$'
+}
 
-if [ -n "$DEBUG" ] && [[ $(echo "$DEBUG" | tr '[:upper:]' '[:lower:]') =~ ^y|yes|1|on$ ]]; then
-  set -xe
-  CHROOTM_EXEC="DEBUG=Y chroot_master.sh"
-else
-  set -e
-  CHROOTM_EXEC="chroot_master.sh"
-fi
+# Set RUN and CHROOTM_EXEC based on flags
+[ "$(check_flag "$PRETEND")" ] && RUN="echo" || RUN=""
+[ "$(check_flag "$DEBUG")" ] && set -xe && CHROOTM_EXEC="DEBUG=Y chroot_master.sh" || CHROOTM_EXEC="chroot_master.sh"
 
 AW_RUN="$RUN ./alpha-wrap/alpha-wrap"
 TEMP_DIR=./build.temp
+ALPINE_SETUP_CMDLINE_FILE="./res/cmdline-alpine_setup.txt"
 
 mkdir -p ${TEMP_DIR}
 
-ALPINE_SETUP_CMDLINE_FILE="./res/cmdline-alpine_setup.txt"
-
+# Check for essential file
 if [ ! -f "${ALPINE_SETUP_CMDLINE_FILE}" ]; then
   echo "File: ${ALPINE_SETUP_CMDLINE_FILE} has not been found"
-  echo "It's essential for a further operations, exiting"
-  return 1
+  echo "It's essential for further operations, exiting"
+  exit 1
 fi
 
 ${AW_RUN} waitfor
 
-# Build Editions:
+# Function to create an edition
+create_edition() {
+    local edition=$1
+    local chroot=$2
+    local device=$3
+    local size=$4
+    local boot_dir="${TEMP_DIR}/${edition}_boot"
+    local image="images/alpbase-${edition}-$(date +%m-%Y_d%d%H%M).iso"
 
-echo "Creating Super Light Edition"
-BUILD_DATE=$(date +%m-%Y_d%d%H%M)
-IMAGE_SL=images/alpbase-super_light-${BUILD_DATE}.iso
+    echo "Creating ${edition^} Edition"
+    ${AW_RUN} extstore add "${image}" "$size"
+    ${AW_RUN} command "/bin/ash -l -c '${CHROOTM_EXEC} exec ${chroot} alpbase_builder.sh ${edition} ${device}'"
 
-${AW_RUN} extstore add ${IMAGE_SL} 450MB
-${AW_RUN} command "/bin/ash -l -c '${CHROOTM_EXEC} exec chroot.armhf alpbase_builder.sh sl /dev/sda'"
+    mount_and_sync "${device}1" "$boot_dir"
+    compress_image "$image"
+}
 
-${AW_RUN} command "/bin/ash -l -c 'mount /dev/sda1 /mnt'"
-mkdir -p ${TEMP_DIR}/sl_boot
-${AW_RUN} sync ${TEMP_DIR}/sl_boot/ /mnt/vmlinuz-rpi -r
-${AW_RUN} sync ${TEMP_DIR}/sl_boot/ /mnt/initramfs-rpi -r
-${AW_RUN} sync ${TEMP_DIR}/sl_boot/ /mnt/cmdline.txt -r
-${AW_RUN} command "/bin/ash -l -c 'umount /mnt'"
+# Function to mount, sync, and unmount
+mount_and_sync() {
+    local device=$1
+    local boot_dir=$2
 
+    ${AW_RUN} command "/bin/ash -l -c 'mount ${device} /mnt'"
+    mkdir -p "$boot_dir"
+    ${AW_RUN} sync "${boot_dir}/" /mnt/vmlinuz-rpi -r
+    ${AW_RUN} sync "${boot_dir}/" /mnt/initramfs-rpi -r
+    ${AW_RUN} sync "${boot_dir}/" /mnt/cmdline.txt -r
+    ${AW_RUN} command "/bin/ash -l -c 'umount /mnt'"
+}
 
-gzip -c ${IMAGE_SL} > ${IMAGE_SL}.gz
-bzip2 -c ${IMAGE_SL} > ${IMAGE_SL}.bz2
-xz -c ${IMAGE_SL} > ${IMAGE_SL}.xz
+# Function to compress images in various formats
+compress_image() {
+    local image=$1
 
+    gzip -c "$image" > "${image}.gz"
+    bzip2 -c "$image" > "${image}.bz2"
+    xz -c "$image" > "${image}.xz"
+}
 
-echo "Creating Just Light Edition"
-BUILD_DATE=$(date +%m-%Y_d%d%H%M)
-IMAGE_JL=images/alpbase-just_light-${BUILD_DATE}.iso
+# Creating Editions
+create_edition "super_light" "chroot.armhf" "/dev/sda" "450MB"
+create_edition "just_light" "chroot.aarch64" "/dev/sdb" "500MB"
+create_edition "bedesktop" "chroot.aarch64" "/dev/sdc" "5200MB"
 
-${AW_RUN} extstore add ${IMAGE_JL} 500MB
-${AW_RUN} command "/bin/ash -l -c '${CHROOTM_EXEC} exec chroot.aarch64 alpbase_builder.sh jl /dev/sdb'"
-
-${AW_RUN} command "/bin/ash -l -c 'mount /dev/sdb1 /mnt'"
-mkdir -p ${TEMP_DIR}/jl_boot
-${AW_RUN} sync ${TEMP_DIR}/jl_boot/ /mnt/vmlinuz-rpi -r
-${AW_RUN} sync ${TEMP_DIR}/jl_boot/ /mnt/initramfs-rpi -r
-${AW_RUN} sync ${TEMP_DIR}/jl_boot/ /mnt/cmdline.txt -r
-${AW_RUN} command "/bin/ash -l -c 'umount /mnt'"
-
-# ${AW_RUN} --device raspi3b ${IMAGE} --imgboot y vmlinuz-rpi initramfs-rpi
-gzip -c ${IMAGE_JL} > ${IMAGE_JL}.gz
-bzip2 -c ${IMAGE_JL} > ${IMAGE_JL}.bz2
-xz -c ${IMAGE_JL} > ${IMAGE_JL}.xz
-
-echo "Creating BeDesktop Edition"
-
-BUILD_DATE=$(date +%m-%Y_d%d%H%M)
-IMAGE_BD=images/alpbase-bedesktop-${BUILD_DATE}.iso
-
-${AW_RUN} extstore add ${IMAGE_BD} 5200MB
-${AW_RUN} command "/bin/ash -l -c '${CHROOTM_EXEC} exec chroot.aarch64 alpbase_builder.sh bd /dev/sdc'"
-
-${AW_RUN} command "/bin/ash -l -c 'mount /dev/sdc1 /mnt'"
-mkdir -p ${TEMP_DIR}/bd_boot
-${AW_RUN} sync ${TEMP_DIR}/bd_boot/ /mnt/vmlinuz-rpi -r
-${AW_RUN} sync ${TEMP_DIR}/bd_boot/ /mnt/initramfs-rpi -r
-${AW_RUN} sync ${TEMP_DIR}/bd_boot/ /mnt/cmdline.txt -r
-${AW_RUN} command "/bin/ash -l -c 'umount /mnt'"
-
-
-gzip -c ${IMAGE_BD} > ${IMAGE_BD}.gz
-bzip2 -c ${IMAGE_BD} > ${IMAGE_BD}.bz2
-xz -c ${IMAGE_BD} > ${IMAGE_BD}.xz
-
-
-# DO tests
-# stop mother machine:
+# Finalization and Testing
 ${AW_RUN} stop
-
-${AW_RUN} --device raspi3b ${IMAGE_SL} \
-          --imgboot n ${TEMP_DIR}/sl_boot/vmlinuz-rpi ${TEMP_DIR}/sl_boot/initramfs-rpi \
-          --cmdline ${ALPINE_SETUP_CMDLINE_FILE}
-
+#${AW_RUN} --device raspi3b "${TEMP_DIR}/sl_boot/vmlinuz-rpi" --imgboot n \
+#          "${TEMP_DIR}/sl_boot/vmlinuz-rpi" "${TEMP_DIR}/sl_boot/initramfs-rpi" \
+#          --cmdline "${ALPINE_SETUP_CMDLINE_FILE}"
 
 exit 0
